8992e41aa724c1582f5771d513d50698aec73b05
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-09-30 10:57:40 +0200

    Add project files.

---
+++ GruppMiniRäknare/Program.cs
@@ -0,0 +1,14 @@
+﻿using GruppMiniRäknare;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+
+Räknare räknare = new Räknare();
+räknare.run();
+
+
+
+//OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+
+
+
+
