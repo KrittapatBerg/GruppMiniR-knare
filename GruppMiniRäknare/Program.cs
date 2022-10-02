00ef4c8e7f08f1b961a20ed30840eb40b054d82f
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-02 23:39:11 +0200

    test

--- GruppMiniRäknare/Program.cs
+++ GruppMiniRäknare/Program.cs
@@ -2,18 +2,5 @@
 using System.Diagnostics;
 using System.Diagnostics.SymbolStore;
 
-//bool on = true;
-//while (on)
-
     Räknare räknare = new Räknare();   //Kör programmet 
     räknare.run();
-
-
-
-
-
-//OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-
-
-
-
